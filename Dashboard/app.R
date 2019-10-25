b250c6341becb8beac3461425b4f9bea7500e783
Tyryn <tyryn.carnegie@gmail.com>
2019-10-25 13:10:28 +0200

    Dashboard commit

---
+++ Dashboard/app.R
@@ -0,0 +1,17 @@
+# Dashboard
+
+library(shiny)
+
+# Define UI for application 
+ui <- fluidPage(
+  
+)
+
+# Define server logic 
+server <- function(input, output) {
+   
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
